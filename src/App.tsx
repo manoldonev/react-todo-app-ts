fdc6f8bbb086ea4eb4f0350f5c6e239ff4545220
Manol Donev <manol.donev@gmail.com>
2022-01-07 17:18:15 +0200

    feat: setup graphql/react-query codegen

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
-const App = (): JSX.Element => <h1 className="text-xl font-bold">Todo App</h1>;
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { Todos } from './components/Todos';
+
+const queryClient = new QueryClient();
+
+const App = (): JSX.Element => (
+  <QueryClientProvider client={queryClient}>
+    <h1 className="text-xl text-center font-bold">Todo App</h1>
+    <div className="flex justify-center">
+      <Todos />
+    </div>
+
+    <ReactQueryDevtools />
+  </QueryClientProvider>
+);
 
 export { App };
