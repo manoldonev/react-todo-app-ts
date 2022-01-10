34061731255dee4a5fe2feea161f0e1352cd2cc9
Manol Donev <manol.donev@gmail.com>
2022-01-10 17:35:53 +0200

    feat: add responsive masonry layout for todo items (#3)

--- src/components/Todos/index.tsx
+++ src/components/Todos/index.tsx
@@ -1,21 +1,37 @@
-import { useTodosQuery } from '../../generated';
+import Masonry from 'react-masonry-component';
+import { convertRemToPixels } from '../../utils';
+import { useTodos } from './useTodos';
 
-const Todos = (): JSX.Element => {
-  const { data, isFetching } = useTodosQuery();
+const Todos = ({ className }: { className?: string }): JSX.Element => {
+  const { data, isFetching, sentryRef } = useTodos();
+  const masonryOptions = {
+    gutter: convertRemToPixels(0.75),
+    stagger: '0.03s',
+    horizontalOrder: false,
+  };
 
   return (
-    <>
-      {isFetching && <p>Loading...</p>}
-      <ul>
-        {data?.todos?.map((todo) => (
-          <li key={todo?.id} className="w-60">
-            <h3>Lorem Ipsum</h3>
-            <p>{todo?.id}</p>
-            <p className="line-clamp-3">{todo?.task}</p>
+    <div className={className}>
+      <Masonry elementType="ul" options={masonryOptions}>
+        {data?.pages.map((page) =>
+          page.todos?.map((todo) => (
+            <li
+              key={todo?.id}
+              className="w-full p-3 mb-3 border rounded-lg xs:w-56 md:w-60 bg-blue-50 border-slate-300"
+            >
+              <h3>Lorem Ipsum</h3>
+              <p>{todo?.id}</p>
+              <p className="line-clamp-3">{todo?.task}</p>
+            </li>
+          )),
+        )}
+        {isFetching && (
+          <li ref={sentryRef}>
+            <h3>Loading...</h3>
           </li>
-        ))}
-      </ul>
-    </>
+        )}
+      </Masonry>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
+    screens: {
+      xs: '480px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [require('@tailwindcss/line-clamp')],
