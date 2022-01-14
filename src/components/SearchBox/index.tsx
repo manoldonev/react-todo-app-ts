7ca3a2fa00918e62d598e750ec43a0f93e1b177b
Manol Donev <manol.donev@gmail.com>
2022-01-14 19:21:31 +0200

    fix: enhance safari px gap hack (#13)

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -7,11 +7,11 @@ const SearchBox = (): JSX.Element => {
         id="search"
         type="search"
         placeholder="Search"
-        className="z-10 w-0 h-12 text-lg duration-300 rounded-l-sm outline-none md:w-[calc(100%_-_3rem)] peer md:px-5 focus:px-5 focus:w-[calc(100%_-_3rem)] pointer-events-auto"
+        className="z-10 w-0 h-12 text-lg duration-300 rounded-l-sm outline-none md:w-[calc(100%_-_3rem+_1px)] peer md:px-5 focus:px-5 focus:w-[calc(100%_-_3rem+_1px)] pointer-events-auto"
       />
       <label
         htmlFor="search"
-        className="p-3 -ml-px bg-white rounded-sm pointer-events-auto select-none peer-focus:rounded-l-none md:rounded-l-none"
+        className="p-3 -ml-px bg-white rounded-sm pointer-events-auto select-none peer-focus:rounded-l-none md:rounded-l-none peer-focus:pointer-events-none"
       >
         <SearchIcon className="w-6 h-6 pointer-events-none" />
       </label>
