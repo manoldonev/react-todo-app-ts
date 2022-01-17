8d09f0924aa5ad611a247463c28f8d235fcccc66
Manol Donev <manol.donev@gmail.com>
2022-01-17 18:26:09 +0200

    fix: remove device specific search appearance on iOS (#15)

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -7,7 +7,7 @@ const SearchBox = (): JSX.Element => {
         id="search"
         type="search"
         placeholder="Search"
-        className="z-10 w-0 h-12 text-lg duration-300 rounded-l-sm outline-none md:w-[calc(100%_-_3rem+_1px)] peer md:px-5 focus:px-5 focus:w-[calc(100%_-_3rem+_1px)] pointer-events-auto"
+        className="z-10 w-0 h-12 text-lg duration-300 rounded-l-sm outline-none md:w-[calc(100%_-_3rem+_1px)] peer md:px-5 focus:px-5 focus:w-[calc(100%_-_3rem+_1px)] pointer-events-auto bg-white"
       />
       <label
         htmlFor="search"
