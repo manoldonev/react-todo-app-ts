3afe8bd83af57160e2e92d2dbfff47a6fc516821
Manol Donev <manol.donev@gmail.com>
2022-01-31 14:21:09 +0200

    refactor: optional className (#26)

--- src/routes/tasks/CtaButton/index.tsx
+++ src/routes/tasks/CtaButton/index.tsx
@@ -1,7 +1,7 @@
 import { PlusIcon } from '@heroicons/react/outline';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-const CtaButton = ({ className }: { className?: string }): JSX.Element => {
+const CtaButton = ({ className = '' }: { className?: string }): JSX.Element => {
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -10,9 +10,7 @@ const CtaButton = ({ className }: { className?: string }): JSX.Element => {
       type="button"
       aria-label="Add new item"
       onClick={() => navigate('/tasks/new', { state: { backgroundLocation: location } })}
-      className={`text-white transition duration-200 ease-in bg-blue-500 rounded-full shadow w-14 h-14 hover:bg-blue-700 active:shadow-lg focus:outline-none ${
-        className ?? ''
-      }`}
+      className={`text-white transition duration-200 ease-in bg-blue-500 rounded-full shadow w-14 h-14 hover:bg-blue-700 active:shadow-lg focus:outline-none ${className}`}
     >
       <PlusIcon />
     </button>
