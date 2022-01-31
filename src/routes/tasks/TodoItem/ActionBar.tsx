ca3a38b185ba96630add27725a9283c146920e92
Manol Donev <manol.donev@gmail.com>
2022-01-31 14:39:49 +0200

    fix: accessible image buttons

--- src/routes/tasks/TodoItem/ActionBar.tsx
+++ src/routes/tasks/TodoItem/ActionBar.tsx
@@ -3,16 +3,16 @@ import { BellIcon, ColorSwatchIcon, PencilAltIcon, TrashIcon } from '@heroicons/
 const ActionBar = ({ className = '', onDelete }: { className?: string; onDelete: () => void }): JSX.Element => {
   return (
     <div className={`flex gap-1 justify-end ${className}`}>
-      <button type="button" className="group" disabled>
+      <button type="button" className="group" disabled aria-label="Set Reminder">
         <BellIcon className="w-6 h-6 p-1 group-disabled:text-slate-300 enabled:hover:border enabled:hover:border-gray-400 rounded-xl enabled:hover:bg-slate-300" />
       </button>
-      <button type="button" className="group" disabled>
+      <button type="button" className="group" disabled aria-label="Background Options">
         <ColorSwatchIcon className="w-6 h-6 p-1 group-disabled:text-slate-300 enabled:hover:border enabled:hover:border-gray-400 rounded-xl enabled:hover:bg-slate-300" />
       </button>
-      <button type="button" className="group" disabled>
+      <button type="button" className="group" disabled aria-label="Edit Item">
         <PencilAltIcon className="w-6 h-6 p-1 group-disabled:text-slate-300 enabled:hover:border enabled:hover:border-gray-400 rounded-xl enabled:hover:bg-slate-300" />
       </button>
-      <button type="button" onClick={onDelete}>
+      <button type="button" onClick={onDelete} aria-label="Delete Item">
         <TrashIcon className="w-6 h-6 p-1 hover:border hover:border-gray-400 rounded-xl hover:bg-slate-300" />
       </button>
     </div>
