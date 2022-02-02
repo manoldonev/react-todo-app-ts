879378a52ebeade9f61f10183785f1d678eef547
Manol Donev <manol.donev@gmail.com>
2022-02-02 15:29:41 +0200

    fix: accessible todo items & toast

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -3,7 +3,13 @@ import toast from 'react-hot-toast';
 
 const queryClient = new QueryClient({
   queryCache: new QueryCache({
-    onError: (error) => toast.error(`Something went wrong: ${(error as Error).message}`),
+    onError: (error) =>
+      toast.error(`Something went wrong: ${(error as Error).message}`, {
+        ariaProps: {
+          role: 'status',
+          'aria-live': 'polite',
+        },
+      }),
   }),
 });
 
