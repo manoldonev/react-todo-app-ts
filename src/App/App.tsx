45d6cac0d8adf5a2e77a6b361c9ada4ae183a2eb
Manol Donev <manol.donev@gmail.com>
2022-02-02 17:24:22 +0200

    feat: routes code-splitting (#32)

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,34 +1,35 @@
-import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
-import { Analytics, Layout, NewTask, NewTaskModal, NotFound, Settings, Tasks } from '../routes';
+import { lazy, Suspense } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorFallback } from './ErrorFallback';
+import { Layout, NotFound } from '../routes';
 import { useScrollToTop } from './useScrollToTop';
+import { LoadingIndicator } from './LoadingIndicator';
+
+const Tasks = lazy(async () => import('../routes/tasks').then((module) => ({ default: module.Tasks })));
+
+const Analytics = lazy(async () => import('../routes/analytics').then((module) => ({ default: module.Analytics })));
+
+const Settings = lazy(async () => import('../routes/settings').then((module) => ({ default: module.Settings })));
 
 const App = (): JSX.Element => {
-  const location = useLocation();
-  const state = location.state as { backgroundLocation?: Location } | null | undefined;
-
   useScrollToTop();
 
   // TODO: animated routes (no good story for v6 atm, see https://github.com/remix-run/react-router/issues/7117#issuecomment-949096628)
   return (
-    <>
-      <Routes location={state?.backgroundLocation ?? location}>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="tasks" />} />
-          <Route path="tasks" element={<Tasks />} />
-          <Route path="tasks/new" element={<NewTask />} />
-          <Route path="analytics" element={<Analytics />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-
-      {/* open route in a modal dialog only if navigating from /tasks */}
-      {state?.backgroundLocation && (
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Suspense fallback={<LoadingIndicator />}>
         <Routes>
-          <Route path="/tasks/new" element={<NewTaskModal />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Navigate to="tasks" />} />
+            <Route path="tasks/*" element={<Tasks />} />
+            <Route path="analytics" element={<Analytics />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
         </Routes>
-      )}
-    </>
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
