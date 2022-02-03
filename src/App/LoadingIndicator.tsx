5187573ad68f52e92c472f73cbd85995ad75220d
Manol Donev <manol.donev@gmail.com>
2022-02-03 12:47:05 +0200

    refactor: add spinner to loading indicator (#35)

--- src/App/LoadingIndicator.tsx
+++ src/App/LoadingIndicator.tsx
@@ -1,3 +1,9 @@
-const LoadingIndicator = (): JSX.Element => <div className="m-4">Loading...</div>;
+import { RefreshIcon } from '@heroicons/react/outline';
+
+const LoadingIndicator = ({ className = '' }: { className?: string }): JSX.Element => (
+  <div className={`m-4 ${className}`}>
+    <RefreshIcon className="inline-block w-6 h-6 animate-spin" /> Loading...
+  </div>
+);
 
 export { LoadingIndicator };
