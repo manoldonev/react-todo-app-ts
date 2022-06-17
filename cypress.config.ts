5d0b4d077898b5c0e757baf200532c562d131ed6
Manol Donev <manol.donev@gmail.com>
2022-06-17 20:56:04 +0300

    chore: migrate to cypress 10

---
+++ cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  projectId: 'rrrdjw',
+  video: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.ts').default(on, config);
+    },
+    baseUrl: 'http://localhost:3000/react-todo-app-ts',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+});
