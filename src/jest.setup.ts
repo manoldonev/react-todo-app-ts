daf913323b83adf1543dd79c0b00e44f00fb9738
Manol Donev <manol.donev@gmail.com>
2022-06-27 17:05:36 +0300

    chore: fix extend-expect typing in test files

--- src/jest.setup.ts
+++ src/jest.setup.ts
@@ -3,6 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 import 'whatwg-fetch';
 import MatchMediaMock from 'jest-matchmedia-mock';
 import { server } from './mocks/msw/server';
